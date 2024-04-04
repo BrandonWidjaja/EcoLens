ee1e7baab6f6cab548e3f1f4df60597636fa40aa
jasonphan118 <phanjp@student.unimelb.edu.au>
2024-04-04 16:04:08 +1100

    add styles

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Card , Button} from '@rneui/themed';
-import tw from 'twrnc';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Card, Button } from '@rneui/themed'
+import tw from 'twrnc'
+import { styles } from './styles.js'
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View style={tw`flex-1 justify-center items-center`}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.container}>
+          Open up App.js to start working on your app!
+        </Text>
         <StatusBar style="auto" />
         <Card>
           <Text>Word of the Day</Text>
@@ -18,19 +21,9 @@ export default function App() {
             well meaning and kindly.
             {'"a benevolent smile"'}
           </Text>
-          <Button title = "Learn More" size="sm" type="clear">
-          </Button>
+          <Button title="Learn More" size="sm" type="clear"></Button>
         </Card>
       </View>
     </SafeAreaProvider>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
