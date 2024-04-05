e43b76791778642e8f0a49b673fc0eb56fef9958
jasonphan118 <phanjp@student.unimelb.edu.au>
2024-04-05 15:42:05 +1100

    main howTo page complete

--- pages/screens/HowToScreen.jsx
+++ pages/screens/HowToScreen.jsx
@@ -2,44 +2,29 @@ import { useEffect } from 'react'
 import { View, ScrollView } from 'react-native'
 import { Card, Text, Header, Icon } from '@rneui/themed'
 import { Image } from 'react-native'
-
+import ImageWithBlackBars from '../../components/ImageWithBlackBars'
 import { styles } from '../../styles'
 const shortText =
-  'In the heart of a bustling city, amidst the rhythmic pulse of life, stories unfold like petals unfurling in the morning sun. Each step on the pavement echoes with whispered tales of triumph, love, and resilience. In the labyrinth of streets, dreams intertwine with reality, painting the urban landscape with hues of ambition and passion. Here, amidst the cacophony of voices and the symphony of honking horns, life dances to its own unique melody, a testament to the infinite possibilities that reside within the beating heart of a metropolis.'
+  'In the heart of a bustling city, amidst the rhythmic pulse of life, stories unfold like petals unfurling in the morning sun. Each step on the pavement echoes with whispered tales of triumph, love, and resilience. In the labyrinth of streets, dreams intertwine with reality, painting the urban landscape with hues of ambition and passion. Here, amidst the cacophony of voices and the symphony of honking horns, life dances to its own unique melody, a testament to the infinite possibilities that reside within the beating heart of a metrasdfjagaogpjkopolis.'
 
 const HowTo = () => {
   return (
     <>
-      <Header
-        containerStyle={{
-          backgroundColor: 'transparent', // Opaque background color
-          borderBottomWidth: 0 // Remove bottom border
-        }}
-        leftComponent={
-          <Icon
-            name="close" // Exit icon name
-            type="material" // Icon type
-            color="black" // Icon color
-            onPress={() => {
-              // Handle exit icon press
-              // Add your navigation logic here
-            }}
-          />
-        }
-      />
-      <ScrollView>
+      <ScrollView style={styles.background}>
         <View style={styles.cardContainer}>
           <Card containerStyle={styles.card}>
-            <Image
-              style={styles.image}
-              source={require('../../assets/Common-wombat.webp')}
-              resizeMode="contain"
-            />
+            <View style={styles.imageContainer}>
+              <Image
+                style={styles.image}
+                source={require('../../assets/Common-wombat.webp')}
+                resizeMode="cover"
+              />
+            </View>
           </Card>
         </View>
-        <Card>
+        <View style={styles.resultBox}>
           <Text style={styles.smallText}>{shortText}</Text>
-        </Card>
+        </View>
       </ScrollView>
     </>
   )
--- styles.js
+++ styles.js
@@ -1,32 +1,45 @@
 import { StyleSheet } from 'react-native'
+const theme = {
+  color: {
+    light: '#FAF3E1',
+    dark: '#023E1A'
+  }
+}
+
+import { Dimensions } from 'react-native'
+
+const windowHeight = Dimensions.get('window').height
 export const styles = StyleSheet.create({
+  background: {
+    backgroundColor: theme.color.light
+  },
+  resultBox: {
+    marginTop: 0.07 * windowHeight,
+    marginLeft: '13%',
+    marginRight: '10%'
+  },
   cardContainer: {
     width: '100%',
+    height: 0.4 * windowHeight,
     alignItems: 'center'
   },
   card: {
+    backgroundColor: '#000000',
     width: '80%',
-    height: 200,
+    height: '100%',
     borderRadius: 10,
     overflow: 'hidden',
-    marginVertical: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
+    margin: 10
+  },
+  imageContainer: {
+    aspectRatio: 16 / 9 // Aspect ratio for a standard image size
   },
   image: {
     width: '100%',
     height: '100%'
   },
   smallText: {
-    color: 'pink',
-    fontSize: 20,
-    fontFamily: 'JosefinSans-Regular', // Apply Josefin Sans font family
-    margin: 10
+    color: theme.dark,
+    fontSize: 20 // Apply Josefin Sans font family
   }
 })
