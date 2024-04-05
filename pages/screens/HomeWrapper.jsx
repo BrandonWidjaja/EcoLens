b7ec1f531bb5540e8a88fc43f90b047023034e6a
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2024-04-06 01:03:32 +1100

    display page from search bar

--- pages/screens/HomeWrapper.jsx
+++ pages/screens/HomeWrapper.jsx
@@ -1,57 +1,58 @@
-import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
-import { useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Home, CameraScreen, Results } from './index.js'
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home, CameraScreen, Results, SearchResults } from "./index.js";
 
 export default function HomeWrapper() {
-  const Stack = createNativeStackNavigator()
-  const [backendText, setBackendText] = useState(null)
+  const Stack = createNativeStackNavigator();
+  const [backendText, setBackendText] = useState(null);
   const handleButtonPress = async () => {
     try {
-      const response = await fetch('http://128.250.0.213:3000/hello')
-      const data = await response.text()
-      console.log(data) // Log the response
-      setBackendText(data) // Set the state with fetched data
+      const response = await fetch("http://128.250.0.213:3000/hello");
+      const data = await response.text();
+      console.log(data); // Log the response
+      setBackendText(data); // Set the state with fetched data
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerShown: false
+          headerShown: false,
         }}
       >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Camera" component={CameraScreen} />
         <Stack.Screen name="Results" component={Results} />
+        <Stack.Screen name="Search" component={SearchResults} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 const styles = StyleSheet.create({
   cameraContainer: {
     flex: 1,
-    width: '100%'
+    width: "100%",
   },
   camera: {
-    flex: 1
+    flex: 1,
   },
   buttonContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
     left: 0,
     right: 0,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- pages/screens/index.js
+++ pages/screens/index.js
@@ -1,6 +1,7 @@
-export { default as Home } from './Home'
-export { default as Info } from './Info'
-export { default as Map } from './Map'
-export { default as CameraScreen } from './CameraScreen'
-export { default as Results } from './Results'
-export { default as Tips } from './Tips'
+export { default as Home } from "./Home";
+export { default as Info } from "./Info";
+export { default as Map } from "./Map";
+export { default as CameraScreen } from "./CameraScreen";
+export { default as Results } from "./Results";
+export { default as Tips } from "./Tips";
+export { default as SearchResults } from "./SearchResults";
