5693bb7ff0a8d73b3409faf6179df10cd026ac55
zimo peng <zimopeng@zimos-Laptop.local>
2024-04-05 15:32:36 +1100

    backend and camera working

--- App.js
+++ App.js
@@ -1,80 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
-import { ScrollView, StyleSheet, Text, View, Image, Button } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Card, Button } from '@rneui/themed'
 import { useState } from 'react'
-import tw from 'twrnc'
 import Home from './pages/screens/Home.jsx'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Page from './pages/screens/Page.jsx'
-import { Camera } from 'expo-camera';
+import CameraScreen from './pages/screens/CameraScreen.jsx'
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   const [backendText, setBackendText] = useState(null);
-  const [cameraPermission, setCameraPermission] = useState(null);
-  const [camera, setCamera] = useState(null);
-  const [imageUri, setImageUri] = useState(null);
-  const [showCamera, setShowCamera] = useState(false);
-
-  const cameraRef = useRef(null);
-
-  const askCameraPermission = async () => {
-    const { status } = await Camera.requestCameraPermissionsAsync();
-    setCameraPermission(status === 'granted');
-  };
-
   const handleButtonPress = async () => {
     try {
-      const response = await fetch('http://10.13.87.253:3000/hello');
+      const response = await fetch('http://128.250.0.213:3000/hello');
       const data = await response.text();
       console.log(data); // Log the response
       setBackendText(data); // Set the state with fetched data
     } catch (error) {
       console.error(error);
     }
-  };
-
-  const takePicture = async () => {
-    if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync();
-      const uri = photo.uri;
-      console.log('Photo URI:', uri);
-      setImageUri(uri);
-      uploadImage(uri);
-    }
-  };
-
-  const uploadImage = async (uri) => {
-    const formData = new FormData();
-    formData.append('image', {
-      uri,
-      name: 'photo.jpg',
-      type: 'image/jpeg',
-    });
-
-    try {
-      const response = await fetch('http://10.13.87.253:3000/upload', {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'content-type': 'multipart/form-data',
-        },
-      });
-      const responseData = await response.json();
-      console.log('Upload Response:', responseData);
-    } catch (error) {
-      console.error('Upload Error:', error);
-    }
-  };
-
-  const openCamera = () => {
-    setShowCamera(true);
-  };
-
-  const closeCamera = () => {
-    setShowCamera(false);
   };
 
   return (
@@ -87,43 +33,9 @@ export default function App() {
             name="Home"
             component={Home}
           />
-          <Stack.Screen name="Profile" component={Page} />
+          <Stack.Screen name="Profile" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-
-    <SafeAreaProvider>
-      <View style={tw`flex-1 justify-center items-center`}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Card>
-          <Text>Word of the Day</Text>
-          {backendText && <Text>{backendText}</Text>}
-          <Button title="Fetch from Backend" size="sm" type="clear" onPress={handleButtonPress} />
-        </Card>
-        {!showCamera && (
-          <Button title="Open Camera" onPress={openCamera} />
-        )}
-        {cameraPermission === null ? (
-          <Button title="Request Camera Permission" onPress={askCameraPermission} />
-        ) : cameraPermission === false ? (
-          <Text>No access to camera</Text>
-        ) : showCamera && (
-          <View style={styles.cameraContainer}>
-            <Camera
-              ref={cameraRef}
-              style={styles.camera}
-              type={Camera.Constants.Type.back}
-              ratio="1:1"
-              onCameraReady={() => setCamera(true)}
-            />
-            <View style={styles.buttonContainer}>
-              <Button title="Take Picture" onPress={takePicture} />
-              <Button title="Take Another Photo" onPress={closeCamera} />
-            </View>
-          </View>
-        )}
-      </View>
-    </SafeAreaProvider>
   );
   
 }
--- pages/screens/CameraScreen.jsx
+++ pages/screens/CameraScreen.jsx
@@ -1,53 +1,115 @@
-import React from 'react'
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Card, Button } from '@rneui/themed'
+import { useState , useRef} from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { Camera } from 'expo-camera';
+import tw from "twrnc";
+
 
 const CameraScreen = () => {
-    const [type, setType] = useState(CameraType.back);
-    const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraPermission, setCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [imageUri, setImageUri] = useState(null);
+  const [showCamera, setShowCamera] = useState(false);
 
-    function toggleCameraType() {
-        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  const cameraRef = useRef(null);
+
+  const askCameraPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setCameraPermission(status === 'granted');
+  };
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      const uri = photo.uri;
+      console.log('Photo URI:', uri);
+      setImageUri(uri);
+      uploadImage(uri);
     }
+  };
 
-    return (
-        <View style={styles.container}>
-        <Camera style={styles.camera} type={type}>
+  const uploadImage = async (uri) => {
+    const formData = new FormData();
+    formData.append('image', {
+      uri,
+      name: 'photo.jpg',
+      type: 'image/jpeg',
+    });
+
+    try {
+      const response = await fetch('http://10.13.201.21:3000/upload', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      });
+      const responseData = await response.json();
+      console.log('Upload Response:', responseData);
+    } catch (error) {
+      console.error('Upload Error:', error);
+    }
+  };
+
+  const openCamera = () => {
+    setShowCamera(true);
+  };
+
+  const closeCamera = () => {
+    setShowCamera(false);
+  };
+
+  return (
+    <SafeAreaProvider>
+      <View style={tw`flex-1 justify-center items-center`}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        {!showCamera && (
+          <Button title="Open Camera" onPress={openCamera} />
+        )}
+        {cameraPermission === null ? (
+          <Button title="Request Camera Permission" onPress={askCameraPermission} />
+        ) : cameraPermission === false ? (
+          <Text>No access to camera</Text>
+        ) : showCamera && (
+          <View style={styles.cameraContainer}>
+            <Camera
+              ref={cameraRef}
+              style={styles.camera}
+              type={Camera.Constants.Type.back}
+              ratio="1:1"
+              onCameraReady={() => setCamera(true)}
+            />
             <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-                <Text style={styles.text}>Flip Camera</Text>
-            </TouchableOpacity>
+              <Button title="Take Picture" onPress={takePicture} />
+              <Button title="Take Another Photo" onPress={closeCamera} />
             </View>
-        </Camera>
-     </View>
-  )
+          </View>
+        )}
+      </View>
+    </SafeAreaProvider>
+  );
 }
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-    camera: {
-      flex: 1,
-    },
-    buttonContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: 'transparent',
-      margin: 64,
-    },
-    button: {
-      flex: 1,
-      alignSelf: 'flex-end',
-      alignItems: 'center',
-    },
-    text: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      color: 'white',
-    },
+  cameraContainer: {
+    flex: 1,
+    width: '100%',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default CameraScreen
