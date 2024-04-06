5207c993a6024037583b72ecfe54811a925443ba
jasonphan118 <phanjp@student.unimelb.edu.au>
2024-04-06 14:19:51 +1100

    pulsing button

---
+++ components/PressableImage.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { StyleSheet, Image, TouchableOpacity, Animated } from 'react-native'
+import { responsiveHeight } from 'react-native-responsive-dimensions'
+const PressableImage = ({ onPress, source }) => {
+  const scaleAnim = new Animated.Value(1)
+
+  useEffect(() => {
+    const pulseAnim = Animated.loop(
+      Animated.sequence([
+        Animated.timing(scaleAnim, {
+          toValue: 1.07,
+          duration: 700,
+          useNativeDriver: true
+        }),
+        Animated.timing(scaleAnim, {
+          toValue: 1,
+          duration: 700,
+          useNativeDriver: true
+        })
+      ])
+    )
+
+    pulseAnim.start()
+
+    return () => {
+      pulseAnim.stop()
+    }
+  }, [])
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Animated.View style={{ transform: [{ scale: scaleAnim }] }}>
+        <Image
+          resizeMode="cover"
+          style={styles.logo}
+          source={require('../assets/logo.jpg')}
+        />
+      </Animated.View>
+    </TouchableOpacity>
+  )
+}
+const styles = StyleSheet.create({
+  logo: {
+    aspectRatio: 1,
+    height: responsiveHeight(30),
+    borderColor: 'green',
+    borderWidth: 9,
+    borderRadius: 100000
+  }
+})
+export default PressableImage
--- pages/screens/Home.jsx
+++ pages/screens/Home.jsx
@@ -1,58 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   StyleSheet,
   TouchableOpacity,
   Image,
   KeyboardAvoidingView,
   ScrollView,
-  Platform,
-} from "react-native";
-import { SearchBar } from "@rneui/themed";
-import { View, Text } from "react-native";
-import { responsiveHeight } from "react-native-responsive-dimensions";
-import axios from "axios";
+  Platform
+} from 'react-native'
+import { SearchBar } from '@rneui/themed'
+import { View, Text } from 'react-native'
+import { responsiveHeight } from 'react-native-responsive-dimensions'
+import axios from 'axios'
+import PressableImage from '../../components/PressableImage.jsx'
 
 const Home = ({ navigation }) => {
-  const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState('')
 
   const updateSearch = (search) => {
-    setSearchText(search);
-  };
+    setSearchText(search)
+  }
 
   const handleSearchSubmit = async () => {
     try {
-      console.log("Search submitted:", searchText);
+      console.log('Search submitted:', searchText)
 
       if (!searchText) {
-        console.error("Search text is empty");
-        return;
+        console.error('Search text is empty')
+        return
       }
 
       const response = await axios.post(
-        "http://" + process.env.EXPO_PUBLIC_MY_IP + ":3000/search",
+        'http://' + process.env.EXPO_PUBLIC_MY_IP + ':3000/search',
         { text: searchText },
         {
           headers: {
-            "Content-Type": "application/json",
-          },
+            'Content-Type': 'application/json'
+          }
         }
-      );
-      console.log("Backend response:", response.data.message);
-      const message = response.data.message;
-      navigation.navigate("Search", { message });
+      )
+      console.log('Backend response:', response.data.message)
+      const message = response.data.message
+      navigation.navigate('Search', { message })
       // Clear search text after successful submission
-      setSearchText("");
+      setSearchText('')
 
       // Handle response data as needed
     } catch (error) {
-      console.error("Error searching text:", error);
+      console.error('Error searching text:', error)
       // Handle error as needed
     }
-  };
+  }
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.container}
     >
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
@@ -64,13 +65,7 @@ const Home = ({ navigation }) => {
             <Text style={styles.desc}>
               Click the button below to scan an Image!
             </Text>
-            <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-              <Image
-                resizeMode="cover"
-                style={styles.logo}
-                source={require("../../assets/logo.jpg")}
-              />
-            </TouchableOpacity>
+            <PressableImage onPress={() => navigation.navigate('Camera')} />
           </View>
           <View style={styles.searchContainer}>
             <Text style={styles.desc}>Or</Text>
@@ -83,7 +78,7 @@ const Home = ({ navigation }) => {
                 round={true}
                 containerStyle={[
                   styles.searchBar,
-                  { borderWidth: 0, borderBottomWidth: 0, borderTopWidth: 0 },
+                  { borderWidth: 0, borderBottomWidth: 0, borderTopWidth: 0 }
                 ]}
                 inputStyle={styles.searchInput}
               />
@@ -99,81 +94,74 @@ const Home = ({ navigation }) => {
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FAF3E1",
+    backgroundColor: '#FAF3E1'
   },
   innerContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20
   },
   titleContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    maxWidth: "80%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '80%'
   },
   contentContainer: {
     flex: 3,
-    justifyContent: "center",
-    alignItems: "center",
-    maxWidth: "80%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '80%'
   },
   searchContainer: {
     flex: 2,
-    width: "80%",
+    width: '80%'
   },
   searchBarContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
   searchBar: {
     flex: 1,
     marginRight: 10,
-    backgroundColor: "#FAF3E1",
-    borderRadius: 10,
+    backgroundColor: '#FAF3E1',
+    borderRadius: 10
   },
   searchInput: {
     fontSize: 16,
-    color: "#333",
+    color: '#333'
   },
   title: {
     fontSize: 70,
     // fontFamily: 'Arial',
-    fontWeight: "500",
-    color: "black",
-    paddingTop: 30,
-  },
-  logo: {
-    aspectRatio: 1,
-    height: responsiveHeight(30),
-    borderColor: "green",
-    borderWidth: 9,
-    borderRadius: 100000,
+    fontWeight: '500',
+    color: 'black',
+    paddingTop: 30
   },
   desc: {
     paddingTop: 20,
     fontSize: 25,
-    textAlign: "center",
-    paddingBottom: 20,
+    textAlign: 'center',
+    paddingBottom: 20
   },
   submitButton: {
-    backgroundColor: "green",
+    backgroundColor: 'green',
     paddingHorizontal: 15,
     paddingVertical: 10,
-    borderRadius: 5,
+    borderRadius: 5
   },
   submitButtonText: {
-    color: "white",
-    fontSize: 18,
-  },
-});
+    color: 'white',
+    fontSize: 18
+  }
+})
 
-export default Home;
+export default Home
